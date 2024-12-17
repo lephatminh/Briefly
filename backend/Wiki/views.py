b0733b404bfbacbe18fb7d9ea29451b264b6505d
Shibainu13 <shibainu.auth@gmail.com>
2024-12-17 10:43:06 +0700

    feat: Fetch article by id backend API

--- backend/Wiki/views.py
+++ backend/Wiki/views.py
@@ -1,10 +1,18 @@
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.db.models import Count
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
-
-MAX_SUGGESTIONS = 6
+from .serializers import SuggestionSerializer, WikiArticleSerializer
+from .models import WikiArticle
+import random
 
 client = Elasticsearch("localhost:9200")
+
+MAX_SUGGESTIONS = 6
 
 def search_suggestions(request):
     prefix = request.GET.get('q', None)
@@ -30,7 +38,7 @@ def search_suggestions(request):
         if 'title_suggestion' in response.suggest:
             for option in response.suggest['title_suggestion'][0].options:
                 if len(seen) <= MAX_SUGGESTIONS:
-                    suggestions.append({
+                    suggestion_data = {
                         'text': option.text,
                         'score': option._score,
                         'post': {    
@@ -41,14 +49,17 @@ def search_suggestions(request):
                                 'alt': option._source.images[0]['alt'],
                             },
                         }
-                    })
+                    }
+                    serializer = SuggestionSerializer(data=suggestion_data)
+                    if serializer.is_valid():
+                        suggestions.append(serializer.validated_data)
                     seen.add(option._source.title)
 
         if len(seen) < MAX_SUGGESTIONS and response.hits:
             for hit in response.hits:
                 option = hit.to_dict()
                 if len(seen) < MAX_SUGGESTIONS and option['title'] not in seen:
-                    suggestions.append({
+                    suggestion_data = {
                         'text': hit.content,
                         'score': hit.meta.score,
                         'post': {  
@@ -56,8 +67,29 @@ def search_suggestions(request):
                             'title': option['title'],
                             'image': option['images'][0],
                         }
-                    })
+                    }
+                    serializer = SuggestionSerializer(data=suggestion_data)
+                    if serializer.is_valid():
+                        suggestions.append(suggestion_data)
+                    seen.add(option['title'])
 
-        return JsonResponse({'suggestions': suggestions})
+        return JsonResponse({'suggestions': suggestions}, status=status.HTTP_200_OK)
 
-    return JsonResponse({'error': 'No prefix provided'}, status=400)
+    return JsonResponse({'error': 'No prefix provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+class WikiArticleDetailView(APIView):
+    def get(self, request, *args, **kwargs):
+        article_id = request.GET.get('id', None)
+        
+        print(article_id)
+        if article_id:
+            article = get_object_or_404(WikiArticle, id=article_id)
+        else:
+            count = WikiArticle.objects.aggregate(count=Count('id'))['count']
+            if count == 0:
+                return JsonResponse({'error': 'No article available'}, status=status.HTTP_404_NOT_FOUND)
+            random_index = random.randint(0, count - 1)
+            article = WikiArticle.objects.all()[random_index]
+            
+        serializer = WikiArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
