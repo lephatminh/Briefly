5aa412cf0c7773b014d36e1e94c119e3e33c263c
lephatminh <22125056@student.hcmus.edu.vn>
2024-12-20 19:31:03 +0700

    Add evaluation code
    Add BARTScore evaluation metric to compute the coverage ratio of the summary

---
+++ evaluation/wiki.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+def create_link(glossary):
+    return "https://en.wikipedia.org/wiki/" + glossary.replace(" ", "_")
+
+def get_glossary_content(glossary):
+    url = create_link(glossary)
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html5lib")
+    glossaryCSS = soup.find("div", {"class": "mw-parser-output"})
+    characterGlossary = glossaryCSS.find_all(class_ = "glossary")
+    link_list = []
+    for character in characterGlossary:
+        keywordList = character.find_all("dt")
+        for keyword in keywordList:
+            link = keyword.find("a")
+            if link:
+                href = link["href"]
+                # Remove the /wiki/ part of the link
+                link_list.append(href[6:])
+    return link_list
+
+def crawl_multiple_glossary(glossaries):
+    wiki_link = []
+    for glossary in glossaries:
+        wiki_link += get_glossary_content(glossary)
+    return wiki_link
+
+glossaries = ["Glossary of artificial intelligence", "Glossary of computer science", "Glossary of civil engineering", 
+            "Glossary of electrical and electronics engineering", "Glossary of mechanical engineering", "Glossary of structural engineering",
+            "Glossary of aerospace engineering", "Glossary of mathematics", "Glossary of physics", "Glossary of areas of mathematics"]
+wiki_terms = crawl_multiple_glossary(glossaries) # Get the list of Wikipedia terms
+wiki_terms = list(set(wiki_terms)) # Remove duplicates
+with open("wiki_terms.txt", "w") as f:
+    for term in wiki_terms:
+        f.write(term + "\n")
