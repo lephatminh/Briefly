8f8267bd328e4d7e6e4c0cd4543833b857be6bb0
Shibainu13 <shibainu.auth@gmail.com>
2024-12-21 10:26:18 +0700

    fix: useSearchParams() should be wrapped in a suspense boundary at page "/article"

--- frontend/app/article/page.tsx
+++ frontend/app/article/page.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import Article from "./Article";
+import { Suspense } from "react";
 
 export default function ArticlePage() {
   return (
     <div className="max-w-full">
       <Header type="logo"/>
-      <Article className="mt-32"/>
+      <Suspense>
+        <Article className="mt-32"/>
+      </Suspense>
       <Footer />
     </div>
   );
